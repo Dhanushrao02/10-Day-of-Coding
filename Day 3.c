a58dce91de98eca4a176a0ddaaedc2bdebda1d18
Dhanushrao02 <84696628+Dhanushrao02@users.noreply.github.com>
2021-11-26 20:01:05 +0530

    Update Day 3.c

--- Day 3.c
+++ Day 3.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+
 int main()
 {
     int i,j,len=0,flag=0;
     char str[100];
+
+
     printf("Enter the string: ");
     scanf("%s",str);
+
+
     len = strlen(str);
+
+
     for(i=0;i<len;i++)
     {
         if(str[i] != str[len-i-1])
@@ -16,10 +23,12 @@ int main()
         }
 
     }
+
         if(flag==0)
         {
             printf("It's palindromic!!!");
         }
+
         else
         {
           printf("It's not palindromic!!!");
