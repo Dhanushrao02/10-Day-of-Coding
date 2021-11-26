4a0edccc3afd05a214560aaaa964134a807e7122
Dhanushrao02 <84696628+Dhanushrao02@users.noreply.github.com>
2021-11-26 20:00:26 +0530

    Update Day 3.c

--- Day 3.c
+++ Day 3.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <string.h>
-void isPalindrome(char str[])
-{
-    int l = 0;
-    int h = strlen(str) - 1;
-    while (h > l)
-    {
-        if (str[l++] != str[h--])
-        {
-            printf("%s is not a palindrome\n", str);
-            return;
-        }
-    }
-    printf("%s is a palindrome\n", str);
-}
 int main()
 {
-    isPalindrome("civic");
-    isPalindrome("lemon");
+    int i,j,len=0,flag=0;
+    char str[100];
+    printf("Enter the string: ");
+    scanf("%s",str);
+    len = strlen(str);
+    for(i=0;i<len;i++)
+    {
+        if(str[i] != str[len-i-1])
+        {
+            flag=1;
+            break;
+        }
+
+    }
+        if(flag==0)
+        {
+            printf("It's palindromic!!!");
+        }
+        else
+        {
+          printf("It's not palindromic!!!");
+        }
+
     return 0;
 }
